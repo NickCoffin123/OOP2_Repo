4b823192f5376acca60031a017dc91262d0d5e42
NickCoffin123 <nicholas.coffin@dcmail.ca>
2024-10-31 10:57:41 -0400

    Base forms made

---
+++ CharacterSheet/Forms/CharacterEdit.cs
@@ -0,0 +1,25 @@
+﻿/* Nick Coffin - 100555045.
+ * OOP - Assignment 3 Character Sheet.
+ * October 31, 2024.
+ * Edit character form for project.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CharacterSheet
+{
+    public partial class CharacterEdit : Form
+    {
+        public CharacterEdit()
+        {
+            InitializeComponent();
+        }
+    }
+}
