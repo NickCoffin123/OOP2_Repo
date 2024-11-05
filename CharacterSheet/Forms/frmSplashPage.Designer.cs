eedf158e9a4f42ec0e4997265c1a82c6cce95cfe
NickCoffin123 <nicholas.coffin@dcmail.ca>
2024-11-04 19:06:02 -0500

    cool stuff

--- CharacterSheet/Forms/frmSplashPage.Designer.cs
+++ CharacterSheet/Forms/frmSplashPage.Designer.cs
@@ -32,13 +32,15 @@
             this.btnEnter = new System.Windows.Forms.Button();
             this.btnExitMain = new System.Windows.Forms.Button();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.lblTitleInfo = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnEnter
             // 
-            this.btnEnter.Location = new System.Drawing.Point(50, 385);
+            this.btnEnter.Location = new System.Drawing.Point(67, 474);
+            this.btnEnter.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnEnter.Name = "btnEnter";
-            this.btnEnter.Size = new System.Drawing.Size(134, 43);
+            this.btnEnter.Size = new System.Drawing.Size(179, 53);
             this.btnEnter.TabIndex = 1;
             this.btnEnter.Text = "&Enter";
             this.toolTip1.SetToolTip(this.btnEnter, "Enter");
@@ -48,27 +50,37 @@
             // btnExitMain
             // 
             this.btnExitMain.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnExitMain.Location = new System.Drawing.Point(654, 385);
+            this.btnExitMain.Location = new System.Drawing.Point(872, 474);
+            this.btnExitMain.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnExitMain.Name = "btnExitMain";
-            this.btnExitMain.Size = new System.Drawing.Size(134, 43);
+            this.btnExitMain.Size = new System.Drawing.Size(179, 53);
             this.btnExitMain.TabIndex = 2;
             this.btnExitMain.Text = "&Exit";
             this.toolTip1.SetToolTip(this.btnExitMain, "Exit");
             this.btnExitMain.UseVisualStyleBackColor = true;
             this.btnExitMain.Click += new System.EventHandler(this.btnExitMain_Click);
             // 
+            // lblTitleInfo
+            // 
+            this.lblTitleInfo.Location = new System.Drawing.Point(47, 13);
+            this.lblTitleInfo.Name = "lblTitleInfo";
+            this.lblTitleInfo.Size = new System.Drawing.Size(100, 50);
+            this.lblTitleInfo.TabIndex = 3;
+            this.lblTitleInfo.Text = "Nick Coffin\r\nOOP - 2\r\nOctober, 31";
+            // 
             // frmSplashPage
             // 
             this.AcceptButton = this.btnEnter;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::CharacterSheet.Properties.Resources.dnd;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.CancelButton = this.btnExitMain;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.lblTitleInfo);
             this.Controls.Add(this.btnExitMain);
             this.Controls.Add(this.btnEnter);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmSplashPage";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Welcome";
@@ -81,6 +93,7 @@
         private System.Windows.Forms.Button btnEnter;
         private System.Windows.Forms.Button btnExitMain;
         private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label lblTitleInfo;
     }
 }
 
