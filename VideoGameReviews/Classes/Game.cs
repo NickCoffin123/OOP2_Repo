390b6aaf815ff27cb32f1e9b4c1979542cfd724b
NickCoffin123 <nicholas.coffin@dcmail.ca>
2024-12-05 08:16:18 -0500

    changes

--- VideoGameReviews/Classes/Game.cs
+++ VideoGameReviews/Classes/Game.cs
@@ -53,6 +53,43 @@ namespace VideoGameReviews.DBAL
 
         #region Static Methods
 
+        public static Game ReturnGame()
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(Settings.Default.dbCon);
+
+                SqlCommand cmd;
+
+                conn.Open();
+
+                cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "SELECT * FROM Games WHERE GameId = @GameId";
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Game g = new Game();
+                    g.GameId = (int)reader["GameId"];
+                    g.Title = reader["Title"].ToString();
+                    g.Genre = reader["Genre"].ToString();
+                    g.ReleaseDate = (DateTime)reader["ReleaseDate"];
+                    games.Add(g);
+
+                    return g;
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            
+        }
+
         public static void PopulateGames()
         {
             try
